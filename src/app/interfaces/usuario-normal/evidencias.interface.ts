9cf072fb45e919b35c7f350f8b6353f07c0015fd
pedroesk <72105328+pedro3sc@users.noreply.github.com>
2022-06-15 15:53:06 -0400

    agregacion en interface
    we add some variables to interface.

--- src/app/interfaces/usuario-normal/evidencias.interface.ts
+++ src/app/interfaces/usuario-normal/evidencias.interface.ts
@@ -1,5 +1,8 @@
 export interface Evidencias{
     id_evidencias: number;
+    nombres_usuario: number;
+    nombre_criterios: number;
+    nombre_debilidades: number;
     nombre_unidad: number;
     nombre_procesos: number;
     nombre_registros: number;
